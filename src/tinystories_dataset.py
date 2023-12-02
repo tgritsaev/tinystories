189081c0b5223c0db6c752c7e19a5e894dac94c8
GritsaevTim <timgri93@gmail.com>
2023-12-03 02:29:56 +0300

    push

--- src/tinystories_dataset.py
+++ src/tinystories_dataset.py
@@ -16,8 +16,8 @@ class TinyStoriesDataset(Dataset):
     def __init__(self, part: str, data_path: str, limit: int = None):
         super().__init__()
         print(f"Loading data from {part}...")
-        self.idxs = np.load(f"{data_path}/{part}_idxs.npy").astype(np.int64)[:limit]
-        self.data = np.load(f"{data_path}/{part}.npy").astype(np.int16)
+        self.idxs = torch.from_numpy(np.load(f"{data_path}/{part}_idxs.npy")).astype(torch.int64)[:limit]
+        self.data = torch.from_numpy(np.load(f"{data_path}/{part}.npy")).astype(torch.int16)
         print(f"Dataset has been created, dataset info:")
         print(f"size:\t{self.idxs.shape[0]}\nmin:\t{self.data.min()}\nmax:\t{self.data.max()}\nshape:\t{self.data.shape}")
 
