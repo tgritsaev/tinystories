badca9c4553e0a9aca6c790bf86df750b3d49fbb
GritsaevTim <timgri93@gmail.com>
2023-12-03 23:05:48 +0300

    push

--- src/model.py
+++ src/model.py
@@ -105,35 +105,43 @@ class PositionalEncoding(nn.Module):
 
 
 class Transformer(nn.Module):
-    def __init__(self, vocab_len: int, d_model: int, nhead: int, dim_feedforward: int, nlayers: int, max_len: int = 400, dropout: float = 0.1):
+    def __init__(self, vocab_len: int, d_model: int, nhead: int, dim_feedforward: int, nlayers: int, max_len: int = 256, dropout: float = 0.1):
         super().__init__()
         self.max_len = max_len
-        self.d_model = d_model
 
-        print(vocab_len, d_model, dim_feedforward)
         self.embedding = nn.Embedding(vocab_len, d_model)
-        self.positional_encoding = PositionalEncoding(d_model, dropout, max_len)
+        self.pos_encoder = PositionalEncoding(d_model, dropout, max_len)
         encoder_layer = nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, nlayers)
-        self.head = nn.Linear(d_model, vocab_len)
+        self.d_model = d_model
+        self.linear = nn.Linear(d_model, vocab_len)
 
         self._init_weights()
 
     def _init_weights(self):
         bound = 0.1
         self.embedding.weight.data.uniform_(-bound, bound)
-        self.head.bias.data.zero_()
-        self.head.weight.data.uniform_(-bound, bound)
+        self.linear.bias.data.zero_()
+        self.linear.weight.data.uniform_(-bound, bound)
 
     def forward(self, src, src_mask=None):
-        src_padding_mask = src == 0
+        """
+        Arguments:
+            src: Tensor, shape ``[batch_size, seq_len, batch_size]``
+            src_mask: Tensor, shape ``[seq_len, seq_len]``
+
+        Returns:
+            output Tensor of shape ``[batch_size, seq_len, ntoken]``
+        """
         src = src.transpose(0, 1)
         src = self.embedding(src) * math.sqrt(self.d_model)
-        src = self.positional_encoding(src)
+        src = self.pos_encoder(src)
         if src_mask is None:
             src_mask = nn.Transformer.generate_square_subsequent_mask(len(src)).to(src.device)
-        output = self.transformer_encoder(src, src_mask, src_padding_mask)
-        return {"logits": self.head(output).transpose(0, 1)}
+        output = self.transformer_encoder(src, src_mask)
+        output = self.linear(output)
+        output = output.transpose(0, 1)
+        return {"logits": output}
 
     @torch.inference_mode()
     def inference(self, prefix: str = "", temp: float = 1.0) -> str:
@@ -142,7 +150,8 @@ class Transformer(nn.Module):
         tokens = [BOS_ID] + text2ids(prefix)
         tokens = torch.tensor(tokens).to(next(self.parameters()).device)
 
-        logits = self.forward(tokens.unsqueeze(0)).transpose(1, 2)["logits"] / temp
+        logits = self.forward(tokens.unsqueeze(0))["logits"].transpose(1, 2) / temp
+
         new_tokens = torch.distributions.categorical.Categorical(logits=logits[:, :, -1]).sample()
         tokens = torch.cat([tokens, new_tokens], dim=0)
 
@@ -150,7 +159,7 @@ class Transformer(nn.Module):
             if new_tokens.item() == EOS_ID:
                 break
 
-            logits = self.forward(tokens.unsqueeze(0)).transpose(1, 2)["logits"] / temp
+            logits = self.forward(tokens.unsqueeze(0))["logits"].transpose(1, 2) / temp
             new_tokens = torch.distributions.categorical.Categorical(logits=logits[:, :, -1]).sample()
             tokens = torch.cat([tokens, new_tokens], dim=0)
 
