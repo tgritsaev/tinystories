eb91db746861d414034f23ebb5ebcdaac1f2b654
GritsaevTim <timgri93@gmail.com>
2023-12-03 14:59:10 +0300

    push

---
+++ test.py
@@ -0,0 +1,34 @@
+import json
+import argparse
+
+import torch
+
+from src.model import Transformer
+
+
+def inference(model):
+    model.eval()
+    print("Write prefix, the model will continue")
+    with torch.no_grad():
+        while prefix := input():
+            print(model.inference(prefix)["text"])
+
+
+def main(args):
+    with open(args.config) as fin:
+        config = json.load(fin)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"device: {device}")
+
+    model = Transformer(**config["model"])
+    model = model.to(device)
+
+    inference(model, device)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", default="configs/test.json", type=str, help="config file path (default: configs/test.json)")
+    args = parser.parse_args()
+    main(args)
